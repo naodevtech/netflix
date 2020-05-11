29af1b2fea487377801687bb9efdb8335c454390
naimtahouri <naimtahouri@gmail.com>
2020-05-11 16:53:06 +0200

    update langages queries

--- apiService.js
+++ apiService.js
@@ -1,12 +1,14 @@
 const API_URL = "https://api.themoviedb.org/3/movie/";
 const API_KEY = "?api_key=e3d354db0d51107a2f24bc1796fb8017";
+const fr = '&language=fr'
+const en = '&language=en'
 
 export default function fetchMovie(movieId) {
-  const url = `${API_URL}${movieId}${API_KEY}`;
+  const url = `${API_URL}${movieId}${API_KEY}${en}`;
   return fetch(url)
     .then(res => { return res.json()})
     .then(movie => {
-    console.log(movie)
+    // console.log(movie)
       return movie;
   })
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fetchMovie from "./apiService.js";
 import Header from "./components/Header.mjs";
 
 (async () => {
-    let movie = await fetchMovie(157336);
+    let movie = await fetchMovie(550);
     console.log(movie)
     document.getElementById("header").innerHTML = Header(movie);
     document.getElementById("header").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
