51a0212893a7151d497472a6d6fdc64d11047761
naimtahouri <naimtahouri@gmail.com>
2020-05-11 19:23:35 +0200

    updating seriesNetflix fetching

---
+++ components/tvShowNetflixRender.mjs
@@ -0,0 +1,6 @@
+export default function tvShowNetflixRender(props) {
+    return `
+    <img src='https://image.tmdb.org/t/p/original/${props[1].poster_path}' />
+          `
+  }
+  
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import fetchMovie from "./apiService.js";
-import Header from "./components/Header.mjs";
+import fetchMovie from "./apiservices/movie.mjs"
+import fetchTvShowNetflix from './apiservices/tvShowNetflix.mjs'
+import Header from "./components/Header.mjs"
+import tvShowNetflixRender from "./components/tvShowNetflixRender.mjs";
 
 (async () => {
-    let movie = await fetchMovie(550);
-    console.log(movie)
+    let movie = await fetchMovie(157336);
+    // console.log(movie)
     document.getElementById("header").innerHTML = Header(movie);
     document.getElementById("header").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
 })();
+
+(async () => {
+    let series = await fetchTvShowNetflix()
+    console.log(series[1].backdrop_path)
+    document.getElementById('tvShowNetflix').innerHTML = tvShowNetflixRender(series)
+})()
