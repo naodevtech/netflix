48d787c8b87f5017f275604ee00667f25f9c4dce
naimtahouri <naimtahouri@gmail.com>
2020-05-13 16:27:22 +0200

    create a crossdiv for quit modal

--- components/modalRender.mjs
+++ components/modalRender.mjs
@@ -1,6 +1,7 @@
 export default function modalRender(props) {
     return `
             <div class="modal__container">
+            <div class="cross">X</div>
               <h2 class="modal__container-heading">${props.name || props.original_title}</h2>
               <div class="modal__container-details">
                 <p class="modal__container-details-p">${Math.ceil(props.popularity)}%</p>
@@ -13,6 +14,6 @@ export default function modalRender(props) {
                 <button class="modal__container-btnFavouriteList"> My List </button>
             </div>
           `;
-  }
+}
 
   
